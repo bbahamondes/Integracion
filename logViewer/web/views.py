afeab7abbc16d9e6d35d9e697993179345c7da1a
Nunoporing <bahamondesbp@gmail.com>
2020-06-25 00:16:48 -0400

    MVP for the website

--- logViewer/web/views.py
+++ logViewer/web/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the web index.")
+    template = loader.get_template('web/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
